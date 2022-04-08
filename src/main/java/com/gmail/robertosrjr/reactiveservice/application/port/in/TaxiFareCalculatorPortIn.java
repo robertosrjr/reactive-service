02d31587938414d15932111213abfc6186067bfd
Roberto S. Ramos Jr <robertosrjr@gmail.com>
2022-04-07 21:30:15 -0300

    resilience4j

--- src/main/java/com/gmail/robertosrjr/reactiveservice/application/port/in/TaxiFareCalculatorPortIn.java
+++ src/main/java/com/gmail/robertosrjr/reactiveservice/application/port/in/TaxiFareCalculatorPortIn.java
@@ -2,11 +2,20 @@ package com.gmail.robertosrjr.reactiveservice.application.port.in;
 
 import com.gmail.robertosrjr.reactiveservice.application.usecase.TaxiFareCalculatorUseCase;
 import com.gmail.robertosrjr.reactiveservice.domain.Taxi;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+/**
+ * Ref.: https://resilience4j.readme.io/docs/getting-started-3
+ *
+ * */
 
 @Slf4j
 @Service
@@ -15,8 +24,21 @@ public class TaxiFareCalculatorPortIn implements TaxiFareCalculatorUseCase {
     @Autowired
     Taxi taxi;
 
+    private final String BACKEND = "TaxiFareCalculator";
+
     @Override
+    @CircuitBreaker(name = BACKEND, fallbackMethod = "fallbackCalculateFare")
+    @RateLimiter(name = BACKEND)
+    @Bulkhead(name = BACKEND)
+    @Retry(name = BACKEND, fallbackMethod = "fallbackCalculateFare")
+    @TimeLimiter(name = BACKEND)
     public Mono<Long> calculateFare(Boolean isNightSurcharge, Long distanceInMile) {
+
+        log.info("calculateFare: isNightSurcharge:{}, distanceInMile:{}", isNightSurcharge, distanceInMile);
+        return this.taxi.calculateFare(isNightSurcharge, distanceInMile);
+    }
+
+    public Mono<Long> fallbackCalculateFare(Boolean isNightSurcharge, Long distanceInMile) {
 
         log.info("calculateFare: isNightSurcharge:{}, distanceInMile:{}", isNightSurcharge, distanceInMile);
         return this.taxi.calculateFare(isNightSurcharge, distanceInMile);
---
+++ src/main/java/com/gmail/robertosrjr/reactiveservice/domain/model/Plate.java
@@ -0,0 +1,5 @@
+package com.gmail.robertosrjr.reactiveservice.domain.model;
+
+public class Plate {
+
+}
